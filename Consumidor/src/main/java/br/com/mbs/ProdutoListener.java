9ad31b4e1fc8c8dc25601e238c15f20f3a296566
Marcelo Barbosa Soares <treinamento@rodosoft.com.br>
2022-09-29 16:25:30 -0300

    melhorias

--- Consumidor/src/main/java/br/com/mbs/ProdutoListener.java
+++ Consumidor/src/main/java/br/com/mbs/ProdutoListener.java
@@ -1,14 +1,18 @@
 package br.com.mbs;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProdutoListener {
 
-	@RabbitListener(queues = {"${queue.produto}"})
+	@Value("${queue}")
+	String queue;
+	
+	@RabbitListener(queues = {"${queue}"})
     public void receive(@Payload String fileBody) {
-		System.out.println("Order: " + fileBody);
+		System.out.println("Mensagem recebida da fila " + queue + ": " +  fileBody);
     }
 }
